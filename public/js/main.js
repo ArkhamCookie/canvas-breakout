ae67637c4419fba65d4a00a6b60f2fb6f8e69ba1
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-05-25 00:05:05 -0500

    Feat: Add collision

--- public/js/main.js
+++ public/js/main.js
@@ -4,16 +4,28 @@ const startButton = document.getElementById('startButton')
 const canvas = document.getElementById('gameCanvas')
 const context = canvas.getContext('2d')
 
+const ballRadius = 10
+
 let ballX = canvas.width / 2
 let ballY = canvas.height - 30
+let directionX = 2
+let directionY = -2
 
 function draw() {
 	context.clearRect(0, 0, canvas.width, canvas.height)
 
 	drawBall(ballX, ballY)
 
-	ballX += 2
-	ballY += -2
+	if (ballX + directionX > canvas.width - ballRadius || ballX + directionX < ballRadius) {
+		directionX = -directionX
+	}
+
+	if (ballY + directionY > canvas.height - ballRadius || ballY + directionY < ballRadius) {
+		directionY = -directionY
+	}
+
+	ballX += directionX
+	ballY += directionY
 }
 
 function startGame() {
