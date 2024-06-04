b0ec57383983d8fd4a75aaad5403c27a7918b000
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-06-03 19:32:04 -0500

    Fix: Simplfy code

--- public/js/main.js
+++ public/js/main.js
@@ -212,7 +212,7 @@ ballColorSelector.addEventListener('change', function (event) {
 
 
 startButton.addEventListener('click', function () {
-	ballColor = document.getElementById('ballColorInput').value
+	ballColor = ballColorSelector.value
 	startGame()
 	startButton.style.display = 'none'
 })
